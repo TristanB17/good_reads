27a5afd4399e033f601c3f8078136beecf3b1857
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-13 10:45:16 -0600

    Basic tests passing - and equals sign caused an inordinate amount of trouble

--- spec/features/user_visits_book_index_page_spec.rb
+++ spec/features/user_visits_book_index_page_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
-describe "User sees all books" do
-  scenario "a user sees all the books" do
-    book_one = Book.create!(title: "My Immortal")
-    book_two = Book.create!(title: "Greatest Fanfic Ever")
+describe "a User" do
+  describe "visiting the index page" do
+    it "sees all the books" do
+      book_one = Book.create!(title: "My Immortal")
+      book_two = Book.create!(title: "Greatest Fanfic Ever")
 
-    visit books_path
+      visit books_path
 
-    expect(page).to have_link(book_1.title)
-    expect(page).to have_link(book_2.title)
+      expect(page).to have_link(book_one.title)
+      expect(page).to have_link(book_two.title)
+    end
+    it "can link to an individual book show page" do
+      book_one = Book.create!(title: "My Immortal")
+      book_two = Book.create!(title: "Greatest Fanfic Ever")
+
+      visit books_path
+      click_link book_one.title
+
+      expect(current_path).to eq(book_path(book_one))
+    end
   end
 end
